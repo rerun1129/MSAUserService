3a98ea4788adb5a606e879993b70fe0c37b60392
KoYunSeok <a01021719359@gmail.com>
2023-07-31 12:25:34 +0900

    유저 조회 API 추가

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -3,13 +3,17 @@ package com.example.userservice.service;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
+import com.example.userservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -31,5 +35,20 @@ public class UserServiceImpl implements UserService{
         userRepository.save ( entity );
 
         return modelMapper.map ( entity, UserDto.class );
+    }
+
+    @Override
+    public UserDto getUserByUserId ( String userId ) {
+        UserEntity userEntity = userRepository.findByUserId ( userId );
+        if(userEntity == null) throw new UsernameNotFoundException ( "User not found" );
+        UserDto userDto = new ModelMapper ( ).map ( userEntity, UserDto.class );
+        List <ResponseOrder> orders = new ArrayList <> ( );
+        userDto.setOrders ( orders );
+        return userDto;
+    }
+
+    @Override
+    public Iterable <UserEntity> getUserByAll ( ) {
+        return userRepository.findAll ();
     }
 }
