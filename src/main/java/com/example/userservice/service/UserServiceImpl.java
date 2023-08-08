9ce1214ecfc29d01ba3c75909256e482eba5d963
rerun1129 <a01021719359@gmail.com>
2023-08-08 23:40:36 +0900

    서킷 브레이커 및 관련 코드 추가

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -30,6 +32,7 @@ public class UserServiceImpl implements UserService {
     private final Environment env;
 //    private final RestTemplate restTemplate;
     private final OrderServiceClient client;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
     @Override
     public UserDto createUser ( UserDto userDto ) {
@@ -67,7 +70,11 @@ public class UserServiceImpl implements UserService {
 //            log.error(ex.getMessage ());
 //        }
             //에러 디코더
-        List <ResponseOrder> orderList = client.getOrders ( userId );
+//        List <ResponseOrder> orderList = client.getOrders ( userId );
+        //서킷 브레이커
+        CircuitBreaker circuitbreaker = circuitBreakerFactory.create ( "circuitbreaker" );
+        List <ResponseOrder> orderList = circuitbreaker.run ( () -> client.getOrders ( userId ),
+                                                                throwable -> new ArrayList <> () );
         userDto.setOrders ( orderList );
         return userDto;
     }
