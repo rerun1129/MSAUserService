142c31a653f9135e97f7fefa4efde20b7990b297
KoYunSeok <a01021719359@gmail.com>
2023-08-09 13:10:46 +0900

    Micrometer 관련 코드 추가

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -26,6 +27,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/health_check")
+    @Timed(value = "users.status", longTask = true)
     public String status (){
         return "It's working in User Service on Port"
                 +", port(local.server.port) = "+ environment.getProperty ( "local.server.port" )
@@ -36,6 +38,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value = "users.welcome", longTask = true)
     public String welcome (){
 //        return environment.getProperty ( "greeting.message" );
         return greeting.getMessage ();
